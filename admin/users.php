38cf86a434f62a87e7e2b9a4eca7a9cf623b5541
MohdRaza216 <mohdrazamoghul2024@gmail.com>
2025-05-28 08:26:56 +0530

    Pagination completed

--- admin/users.php
+++ admin/users.php
@@ -22,7 +22,10 @@
                         <?php
 
                         include "config.php";
-                        $sql = "SELECT * FROM user ORDER BY user_id DESC";
+                        $limit = 3;
+                        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+                        $offset = ($page - 1) * $limit;
+                        $sql = "SELECT * FROM user ORDER BY user_id LIMIT {$offset}, {$limit}";
                         $result = mysqli_query($conn, $sql) or die('Query Failed!');
                         if (mysqli_num_rows($result) > 0) {
                             while ($row = mysqli_fetch_assoc($result)) {
@@ -30,17 +33,19 @@
 
                                 <tr>
                                     <td class='id'><?php echo $row['user_id'] ?></td>
-                                    <td><?php echo $row['first_name'] ?> <?php echo $row['last_name'] ?></td>
+                                    <td><?php echo $row['first_name'] ?>         <?php echo $row['last_name'] ?></td>
                                     <td><?php echo $row['username'] ?></td>
-                                    <td><?php 
-                                    if($row['role'] == 1){
+                                    <td><?php
+                                    if ($row['role'] == 1) {
                                         echo "Admin";
                                     } else {
                                         echo "Normal";
                                     }
                                     ?></td>
-                                    <td class='edit'><a href='update-user.php?id=<?php echo $row['user_id'] ?>'><i class='fa fa-edit'></i></a></td>
-                                    <td class='delete'><a href='delete-user.php?id=<?php echo $row['user_id'] ?>'><i class='fa fa-trash-o'></i></a></td>
+                                    <td class='edit'><a href='update-user.php?id=<?php echo $row['user_id'] ?>'><i
+                                                class='fa fa-edit'></i></a></td>
+                                    <td class='delete'><a href='delete-user.php?id=<?php echo $row['user_id'] ?>'><i
+                                                class='fa fa-trash-o'></i></a></td>
                                 </tr>
                                 <?php
                             }
@@ -48,11 +53,41 @@
                         ?>
                     </tbody>
                 </table>
-                <ul class='pagination admin-pagination'>
-                    <li class="active"><a>1</a></li>
-                    <li><a>2</a></li>
-                    <li><a>3</a></li>
-                </ul>
+                <?php
+                $sql1 = "SELECT * FROM user";
+                $result1 = mysqli_query($conn, $sql1) or die("Connection Failed");
+
+                if (mysqli_num_rows($result1) > 0) {
+                    $total_records = mysqli_num_rows($result1);
+
+                    
+
+                    $total_pages = ceil($total_records / $limit);
+
+                    echo "<ul class='pagination admin-pagination'>";
+                    if($page > 1){
+                    echo '<li><a href="users.php?page='. ($page - 1).'">Previous</a></li>';
+
+                    }
+                    for ($i = 1; $i <= $total_pages; $i++) {
+                        if($i == $page){
+                            $active = "active";
+                        } else {
+                            $active = "";
+                        }
+                        echo '<li class="'. $active .'"><a href="users.php?page=' . $i . '">' . $i . '</a></li>';
+
+                    }
+                    if($total_pages > $page){
+                    echo '<li><a href="users.php?page='. ($page + 1).'">Next</a></li>';
+
+                    }
+                    echo "</ul>";
+                }
+                ?>
+
+                <!-- <li class="active"><a>1</a></li> -->
+
             </div>
         </div>
     </div>
